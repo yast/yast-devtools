342f0c663483166cdc332a9c5928588494153ff4
Michal Švec <msvec@suse.cz>
2004-02-05 11:29:30 +0000

    - NI updates and beautyfication - added minimal testsuite
    svn path=/trunk/devtools/; revision=13879

---
+++ devtools/skeletons/config/src/XXPkgXX.pm
@@ -0,0 +1,13 @@
+#! /usr/bin/perl -w
+#
+# File:		modules/XXPkgXX.pm
+# Package:	Configuration of XXpkgXX
+# Summary:	XXPkgXX settings, input and output functions
+# Authors:	XXmaintainerXX <XXemailXX>
+#
+# $Id$
+#
+# Representation of the configuration of XXpkgXX.
+# Input and output routines.
+
+# EOF

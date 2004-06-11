2a1681ff267c931b270250744ecd3ce52b31f674
Martin Vidner <mvidner@suse.cz>
2004-06-11 14:59:45 +0000

    updated graphs from 9.1 yet better script, works for Perl too
    svn path=/trunk/devtools/; revision=17806

--- misc/import.pl
+++ misc/import.pl
@@ -1,9 +1,47 @@
 #! /usr/bin/perl
-# parameters: none
+
+=head1 NAME
+
+import.pl - makes graphs of YaST dependencies
+
+=head1 SYNOPSIS
+
+  import.pl -h|--help|--man
+  import.pl {--modules [--clusters] | --packages} [--y2dir <y2dir>] [--topdown]
+
+=head1 AUTHOR
+
+Martin Vidner <mvidner@suse.cz>
+
+=cut
+
 use strict;
 use warnings;
 use diagnostics;
 use IO qw(File);
+use Getopt::Long;
+use Pod::Usage;
+
+my $help = 0;
+my $man = 0;
+# what should be output
+my ($modgraph, $clusters) = (0, 0);
+my $pkggraph = 0;
+my $y2dir = "/usr/share/YaST2";
+my $topdown = 0;
+
+Getopt::Long::Configure ("bundling");
+GetOptions (
+	    "help|h" => \$help,
+	    "man" => \$man,
+	    "modules|m" => \$modgraph,
+	    "clusters|c" => \$clusters,
+	    "packages|p" => \$pkggraph,
+	    "y2dir|d=s" => \$y2dir,
+	    "topdown|t" => \$topdown,
+	   ) or pod2usage (2);
+pod2usage (1) if $help;
+pod2usage (-exitstatus => 0, -verbose => 2) if $man;
 
 # prevent include cycles :-(
 #my %includes;
@@ -27,19 +65,25 @@ sub get_imports ($$)
 	return ();
     }
 
-    my $F = new IO::File ($pathname);
+    my $F = new IO::File ($pathname) or die "Cannot open $pathname: $!";
     # we have seen it already
     $r_includes->{$pathname} = 1;
 
     while (defined ($_ = <$F>))
     {
+	# YCP import
 	if (m/^\s*import\s*"(.*)";/)
 	{
 	    push @result, $1;
 	}
+	# Perl import
+	elsif (m/^\s*YaST::YCP::Import\s*\(?\s*"(.*)"\)?;/)
+	{
+	    push @result, $1 unless ($1 eq "SCR");
+	}
 	elsif (m/^\s*include\s*"(.*)";/)
 	{
-	    my $inc = "/usr/share/YaST2/include/$1";
+	    my $inc = "$y2dir/include/$1";
 	    push (@result, get_imports ($inc, $r_includes));
 	}
     }
@@ -48,26 +92,22 @@ sub get_imports ($$)
     return @result;
 }
 
-# what should be output
-my ($modgraph, $clusters) = (0, 0);
-my $pkggraph = 1;
-
 print "digraph \"import\" {\n";
-print "rankdir=LR;\n";
+print "rankdir=LR;\n" unless ($topdown);
 print "size=\"16,11\"; rotate=90;\n";
 my %pkg2mod = (); # hash of lists
 my %mod2pkg = (); # hash of strings
 my %dep = (); # hash of lists
-foreach (glob ("/usr/share/YaST2/modules/*.ycp"))
+foreach (glob ("$y2dir/modules/{*,*/*}.{ycp,pm}"))
 {
     my $module = $_;
     my $rpm = `rpm -qf --qf \%{name} $module`;
 #    chomp $rpm;
 #    $rpm =~ s/[0-9.-]*$//; 
     chomp $rpm;
-    $module =~ s:.*/(.*)\.ycp:$1:;
+    $module =~ s:$y2dir/modules/(.*)\.[^.]*:$1:;
+    $module =~ s{/}{::}g;
     print STDERR "M $module\n";
-    print STDERR "P $rpm\n";
     $mod2pkg{$module} = $rpm;
     push @{$pkg2mod{$rpm}}, $module;
 
--- misc/import.sh
+++ misc/import.sh
@@ -1,4 +1,7 @@
-#! /bin/bash
-./import.pl > import-notr.dot
-tred import-notr.dot > import.dot
+#! /bin/bash -e
+./import.pl -m > import.notr.dot
+./import.pl -p > import-pkg.notr.dot
+./import.pl -m -c > import-clu.notr.dot
 make import.ps
+make import-pkg.ps
+make import-clu.ps

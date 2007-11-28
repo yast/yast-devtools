a47920100acc802377e8bb03bbdc6e9c8483fd08
Duncan Mac-Vicar <dmacvicar@suse.de>
2007-11-28 20:55:00 +0000

    Some additional variables to make the spec file work
    svn path=/trunk/devtools/; revision=42481

--- devtools/cmake/modules/YastCommon.cmake
+++ devtools/cmake/modules/YastCommon.cmake
@@ -5,11 +5,11 @@
 SET(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/modules ${CMAKE_MODULE_PATH})
 #SET(CMAKE_MODULE_PATH ${CMAKE_INSTALL_PREFIX}/share/cmake/Modules ${CMAKE_MODULE_PATH})
 
-IF (NOT DEFINED PACKAGE)
-  FILE(READ "${CMAKE_SOURCE_DIR}/RPMNAME" PACKAGE)
-ENDIF (NOT DEFINED PACKAGE)
+IF (NOT DEFINED RPMNAME)
+  FILE(READ "${CMAKE_SOURCE_DIR}/RPMNAME" RPMNAME)
+ENDIF (NOT DEFINED RPMNAME)
 
-MESSAGE(STATUS "package name set to '${PACKAGE}'")
+MESSAGE(STATUS "package name set to '${RPMNAME}'")
 INCLUDE(${CMAKE_SOURCE_DIR}/VERSION.cmake)
 SET ( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}" )
 
@@ -17,7 +17,42 @@ SET ( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}" )
 # RPM SPEC                                                         #
 ####################################################################
 MESSAGE(STATUS "Writing spec file...")
-CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/${PACKAGE}.spec.in ${CMAKE_BINARY_DIR}/package/${PACKAGE}.spec @ONLY)
+
+SET(HEADER-COMMENT
+"#
+# spec file for package ${RPMNAME} (Version ${VERSION})
+#
+# norootforbuild",
+#"/work/built/info/failed/
+)
+
+SET(HEADER
+"Name:           ${RPMNAME}
+Version:        ${VERSION}
+Release:        0
+License:        GPL
+Group:          System/YaST
+BuildRoot:      %{_tmppath}/%{name}-%{version}-build\n
+Source0:        ${RPMNAME}-${VERSION}.tar.bz2"
+)
+
+SET(PREP
+"%prep
+%setup -n ${RPMNAME}-${VERSION}"
+)
+
+SET(CLEAN
+"%clean
+rm -rf \"\$RPM_BUILD_ROOT\""
+)
+
+
+SET(INSTALL
+"%install
+make install DESTDIR=\"$RPM_BUILD_ROOT\""
+)
+
+CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/${RPMNAME}.spec.in ${CMAKE_BINARY_DIR}/package/${RPMNAME}.spec @ONLY)
 ####################################################################
 
 SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "-")
@@ -28,7 +63,7 @@ SET(CPACK_PACKAGE_VERSION_PATCH ${VERSION_PATCH})
 
 SET( CPACK_GENERATOR "TBZ2")
 SET( CPACK_SOURCE_GENERATOR "TBZ2")
-SET( CPACK_SOURCE_PACKAGE_FILE_NAME "${PACKAGE}-${VERSION}" )
+SET( CPACK_SOURCE_PACKAGE_FILE_NAME "${RPMNAME}-${VERSION}" )
 
 # The following components are regex's to match anywhere (unless anchored)
 # in absolute path + filename to find files or directories to be excluded
@@ -52,7 +87,7 @@ SET( AUTOBUILD_COMMAND
   COMMAND ${CMAKE_MAKE_PROGRAM} package_source
   COMMAND ${CMAKE_COMMAND} -E copy ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.bz2 ${CMAKE_BINARY_DIR}/package
   COMMAND ${CMAKE_COMMAND} -E remove ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.bz2
-  COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/package/${PACKAGE}.changes" "${CMAKE_BINARY_DIR}/package/${PACKAGE}.changes"
+  COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/package/${RPMNAME}.changes" "${CMAKE_BINARY_DIR}/package/${RPMNAME}.changes"
 )
 
 ADD_CUSTOM_TARGET( srcpackage_local

8d9849fcc133cad933147cbb9b4870714b8080bf
Josef Reidinger <jreidinger@suse.cz>
2013-09-17 17:45:51 +0200

    remove graph generator as ycp is no longer live

--- misc/import.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -e
-MKDOT=../devtools/bin/ycpmakedep
-$MKDOT -m > import.notr.dot
-$MKDOT -p > import-pkg.notr.dot
-$MKDOT -m -c > import-clu.notr.dot
-make import.ps
-make import-pkg.ps
-make import-clu.ps

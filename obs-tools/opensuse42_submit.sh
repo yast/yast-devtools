440862dea9233864e26b20615c380cf0ca0ccc95
Josef Reidinger <jreidinger@suse.cz>
2015-07-29 13:53:20 +0200

    compare by version

--- obs-tools/opensuse42_submit.sh
+++ obs-tools/opensuse42_submit.sh
@@ -4,8 +4,8 @@ LIST=`osc list openSUSE:Factory | grep -i -e yast2 -e skelcd -e "^libstorage$" -
 
 for i in $LIST; do
   # compare if something is new by comparing binaries ( as version is part of rpm output )
-  OS42=$(osc list -b openSUSE:42 $i -r standard -a x86_64)
-  FACTORY=$(osc list -b openSUSE:Factory $i -r standard -a x86_64)
+  OS42=$(osc cat openSUSE:42 $i $i.spec 2>/dev/null | grep "^Version" | sed 's/Version:[[:space:]]*//;s/#.*$//')
+  OS42=$(osc cat openSUSE:Factory $i $i.spec 2>/dev/null | grep "^Version" | sed 's/Version:[[:space:]]*//;s/#.*$//')
   if [ "$OS42" == "$FACTORY" ]; then
      continue
   fi

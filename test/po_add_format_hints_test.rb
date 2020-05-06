8cfc675074745ab5618db13c8a4555d69e89860d
Martin Vidner <martin@vidner.net>
2020-05-06 15:54:26 +0200

    Find the data file even from outside of test/

--- test/po_add_format_hints_test.rb
+++ test/po_add_format_hints_test.rb
@@ -92,17 +92,17 @@ describe Yast::GetText::Tools::PoAddFormatHints do
   describe "#run" do
     # Always use "--dry-run" to avoid modifying the sample .pot file!
     it "Reports the expected number of changes" do
-      argv = ["--dry-run", "data/hello.pot"]
+      argv = ["--dry-run", "#{__dir__}/data/hello.pot"]
       expect { subject.run(argv) }.to output(/^.*hello.pot: 4 format flags added$/).to_stdout
     end
 
     it "Reports one less change when disabling perl-brace-format" do
-      argv = ["--dry-run", "--no-perl-brace-format", "data/hello.pot"]
+      argv = ["--dry-run", "--no-perl-brace-format", "#{__dir__}/data/hello.pot"]
       expect { subject.run(argv) }.to output(/3 format flags added/).to_stdout
     end
 
     it "Reports the added formats in verbose mode" do
-      argv = ["--dry-run", "--verbose", "data/hello.pot"]
+      argv = ["--dry-run", "--verbose", "#{__dir__}/data/hello.pot"]
       added = /Adding c-format.*Adding c-format.*Adding ycp-format.*Adding perl-brace-format/m
       expect { subject.run(argv) }.to output(added).to_stdout
     end
